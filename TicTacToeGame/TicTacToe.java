c22ebd487c5450cf4db791c98369ce9fd8b86c56
Harlan Ferguson <harlanjazz@gmail.com>
2023-03-26 20:49:50 -0400

    working console app

---
+++ TicTacToeGame/TicTacToe.java
@@ -0,0 +1,114 @@
+public class TicTacToe {
+    private char[][] board;
+    private static final char EMPTY = '-'; //final allows for EMPTY to be constant. use to fill board at beginning
+
+    public TicTacToe(){
+        //when initializing the game, every position on board set to EMPTY (-)
+        board = new char[3][3];
+        for(int x = 0; x < 3; x++){
+            for(int y = 0; y < 3; y++){
+                board[x][y] = EMPTY;
+            }
+        }
+    }
+
+    //check that move is within bounds and on an available space
+    public boolean play(char symbol, int row, int col) {
+        if (row >= 0 && row < 3 && col >= 0 && col < 3 && board[row][col] == EMPTY) {
+            board[row][col] = symbol;
+            return true;
+        }
+        return false;
+    }
+
+    //there are 8 ways to win. Fill a column, fill a row, or fill a diagonal. 
+    public boolean checkWin(char symbol) {
+        for (int row = 0; row < 3; row++) {
+            if ((board[row][0] == symbol && board[row][1] == symbol && board[row][2] == symbol) ||
+                (board[0][row] == symbol && board[1][row] == symbol && board[2][row] == symbol)) {
+                return true;
+            }
+        }
+    
+        // Check the top left to bottom right diagonal
+        if (board[0][0] == symbol && board[1][1] == symbol && board[2][2] == symbol) {
+            return true;
+        }
+    
+        // Check the top right to bottom left diagonal
+        if (board[0][2] == symbol && board[1][1] == symbol && board[2][0] == symbol) {
+            return true;
+        }
+    
+        return false;
+    }
+
+    public boolean checkDraw() {
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                if (board[row][col] == EMPTY) {
+                    return false;
+                }
+            }
+        }
+        return true; //if the board is filled and checkWin returned false, game is a draw
+    }
+
+    public int[] findBestMove(char symbol) {
+        int bestScore = Integer.MIN_VALUE;
+        int[] bestMove = new int[2];
+
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                if (board[row][col] == EMPTY) {
+                    board[row][col] = symbol;
+                    int score = minimax(false, symbol);
+                    board[row][col] = EMPTY;
+
+                    if (score > bestScore) {
+                        bestScore = score;
+                        bestMove[0] = row;
+                        bestMove[1] = col;
+                    }
+                }
+            }
+        }
+        return bestMove;
+    }
+
+    private int minimax(boolean maximizing, char symbol) {
+        char player2Symbol = (symbol == 'X') ? 'O' : 'X'; //if p1 symbol x, assign o to p2 else assign x to p2.
+
+        if (checkWin(symbol)) {
+            return 1;
+        } else if (checkWin(player2Symbol)) {
+            return -1;
+        } else if (checkDraw()) {
+            return 0;
+        }
+
+        int bestScore = maximizing ? Integer.MIN_VALUE : Integer.MAX_VALUE; //
+
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                if (board[row][col] == EMPTY) {
+                    board[row][col] = maximizing ? symbol : player2Symbol;
+                    int score = minimax(!maximizing, symbol);
+                    board[row][col] = EMPTY;
+
+                    bestScore = maximizing ? Math.max(score, bestScore) : Math.min(score, bestScore);
+                }
+            }
+        }
+        return bestScore;
+    }
+
+    public void printBoard() {
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                System.out.print(board[row][col] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ TicTacToeGame/main.java
@@ -0,0 +1,91 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class main {
+    public static void main(String[] args){
+        TicTacToe game = new TicTacToe();
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        System.out.println("Please enter player 1's name: ");
+        String player1Name = scanner.nextLine();
+        
+        System.out.println("Please enter player 2's name. Please type: AI if you want to play against the AI: ");
+        String player2Name = scanner.nextLine();
+
+        System.out.println("Please choose " + player1Name + "'s symbol either 'X' or 'O'");
+        char player1Symbol = scanner.next().toUpperCase().charAt(0);
+        char player2Symbol = (player1Symbol == 'X')? 'O' : 'X'; //if p1 symbol x, assign o to p2 else assign x to p2.
+
+        int difficulty = 0;
+
+        if(player2Name.equalsIgnoreCase("AI")){
+            System.out.println("Please choose AI difficulty. Type 1 for randomized, 2 for minimax");
+            difficulty = scanner.nextInt();
+        }
+
+        //end of user input setups
+
+        boolean isGameOver = false;
+        boolean player1Turn = true;
+
+        while(!isGameOver){
+            game.printBoard();
+
+            if (player1Turn) {
+                System.out.println(player1Name + "'s turn.");
+            } else {
+                System.out.println(player2Name + "'s turn.");
+            }
+
+            if (!player2Name.equalsIgnoreCase("AI") || player1Turn) {
+                System.out.println("Enter row and column (1-3) separated by space: ");
+                int row = scanner.nextInt() - 1;
+                int col = scanner.nextInt() - 1;
+                char currentPlayerSymbol = player1Turn ? player1Symbol : player2Symbol;
+
+                while (!game.play(currentPlayerSymbol, row, col)) {
+                    System.out.println("Invalid move. Try again.");
+                    row = scanner.nextInt() - 1;
+                    col = scanner.nextInt() - 1;
+                }
+
+                if (game.checkWin(currentPlayerSymbol)) {
+                    game.printBoard();
+                    System.out.println((player1Turn ? player1Name : player2Name) + " wins!");
+                    isGameOver = true;
+                } else if (game.checkDraw()) {
+                    game.printBoard();
+                    System.out.println("It's a draw!");
+                    isGameOver = true;
+                } else {
+                    player1Turn = !player1Turn;
+                }
+            } else {
+                if (difficulty == 1) {
+                    int row, col;
+                    do {
+                        row = random.nextInt(3);
+                        col = random.nextInt(3);
+                    } while (!game.play(player2Symbol, row, col));
+                } else { //implement minimax
+                    int[] move = game.findBestMove(player2Symbol);
+                    game.play(player2Symbol, move[0], move[1]);
+                }
+
+                if (game.checkWin(player2Symbol)) {
+                    game.printBoard();
+                    System.out.println("Computer wins!");
+                    isGameOver = true;
+                } else if (game.checkDraw()) {
+                    game.printBoard();
+                    System.out.println("It's a draw!");
+                    isGameOver = true;
+                } else {
+                    player1Turn = !player1Turn;
+                }
+            }          
+        }
+        scanner.close();     
+    }
+}
