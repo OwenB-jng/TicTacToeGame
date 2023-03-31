a53c50e7a5e8a4554d571c09e62c1b89f61dbef8
OwenB-jng <owenbeattie1@gmail.com>
2023-03-31 16:24:27 -0400

    gui time

--- TicTacToeGame/GUI.java
+++ TicTacToeGame/GUI.java
@@ -15,6 +15,8 @@ public class GUI extends JFrame implements ActionListener {
     public boolean gameDone = false;
     public int buttonX = 0;
     public int buttonY = 0;
+    public Label victor = new Label("");
+    public Label playerTurn;
 
     public GUI(TicTacToe gameBoard, String player1Name, String player2Name, char player1Symbol, char player2Symbol, boolean player1Turn){
         this.gameBoard = gameBoard;
@@ -26,6 +28,7 @@ public class GUI extends JFrame implements ActionListener {
         Dimension screenDimension = Toolkit.getDefaultToolkit().getScreenSize();
         int xMid = (screenDimension.width - 500) / 2;
         int yMid = (screenDimension.height - 500) / 2;
+
         JFrame frame = new JFrame();
         JPanel buttonPanel = new JPanel();
         buttonPanel.setLayout(new GridLayout(4,3));
@@ -37,6 +40,7 @@ public class GUI extends JFrame implements ActionListener {
         for (int i = 0; i < 3; i++){
             for (int j = 0; j < 3; j++){
                 buttons[i][j] = new JButton("");
+                buttons[i][j].setFont(new Font(Font.SANS_SERIF, Font.BOLD,50));
                 buttons[i][j].addActionListener(this);
                 buttonPanel.add(buttons[i][j]);
             }
@@ -55,50 +59,137 @@ public class GUI extends JFrame implements ActionListener {
         if (!(button.getText() == "")){
             return;
         }
+        // PVE
+        if (player2Name.equalsIgnoreCase("ai")){
 
-        while (!gameDone){
-            if (player1Turn){
-                button.setText(String.valueOf(player1Symbol));
-                while (!buttonFound){
-                    if (button == buttons[buttonX][buttonY]){
-                        gameBoard.play(player1Symbol, buttonX, buttonY);
-                        buttonFound = true;
-                    }
-                    else{
-                        buttonY++;
-                        if (buttonY == 3){
-                            buttonX++;
-                            buttonY = 0;
+            while (!gameDone){
+                if (player1Turn){
+                    button.setText(String.valueOf(player1Symbol));
+                    while (!buttonFound){
+                        if (button == buttons[buttonX][buttonY]){
+                            gameBoard.play(player1Symbol, buttonX, buttonY);
+                            buttonFound = true;
+                        }
+                        else{
+                            buttonY++;
+                            if (buttonY == 3){
+                                buttonX++;
+                                buttonY = 0;
+                            }
                         }
                     }
+                    buttonY = 0;
+                    buttonX = 0;
+                    buttonFound = false;
+                    player1Turn = false;
+                    if (gameBoard.checkWin(player1Symbol)){
+                        gameDone = true;
+                        victor.setText("The winner is " + player1Name + "!");
+                    }
                 }
-                buttonY = 0;
-                buttonX = 0;
-                buttonFound = false;
-                player1Turn = false;
-                if (gameBoard.checkWin(player1Symbol)) gameDone = true;
+                else{
+                    button.setText(String.valueOf(player2Symbol));
+                    while (!buttonFound){
+                        if (button == buttons[buttonX][buttonY]){
+                            gameBoard.play(player2Symbol, buttonX, buttonY);
+                            buttonFound = true;
+                        }
+                        else{
+                            buttonY++;
+                            if (buttonY == 3){
+                                buttonX++;
+                                buttonY = 0;
+                            }
+                        }
+                    }
+                    buttonY = 0;
+                    buttonX = 0;
+                    buttonFound = false;
+                    player1Turn = true;
+                    if (gameBoard.checkWin(player2Symbol)){
+                        gameDone = true;
+                        victor.setText("The winner is " + player2Name + "!");
+                    }
+                }
+                if (gameDone){
+                    JFrame winnerFrame = new JFrame();
+                    JPanel victorPanel = new JPanel();
+                    victorPanel.add(victor);
+                    winnerFrame.add(victorPanel);
+                    Dimension screenDimension = Toolkit.getDefaultToolkit().getScreenSize();
+                    int xMid = (screenDimension.width - 300) / 2;
+                    int yMid = (screenDimension.height - 100) / 2;
+                    winnerFrame.setSize(300,100);
+                    winnerFrame.setLocation(xMid, yMid);
+                    winnerFrame.setVisible(true);
+                }
                 return;
             }
-            else{
-                button.setText(String.valueOf(player2Symbol));
-                while (!buttonFound){
-                    if (button == buttons[buttonX][buttonY]){
-                        gameBoard.play(player2Symbol, buttonX, buttonY);
-                        buttonFound = true;
-                    }
-                    else{
-                        buttonY++;
-                        if (buttonY == 3){
-                            buttonX++;
-                            buttonY = 0;
+
+        }
+        // PVP
+        else{
+            while (!gameDone){
+                if (player1Turn){
+                    button.setText(String.valueOf(player1Symbol));
+                    while (!buttonFound){
+                        if (button == buttons[buttonX][buttonY]){
+                            gameBoard.play(player1Symbol, buttonX, buttonY);
+                            buttonFound = true;
+                        }
+                        else{
+                            buttonY++;
+                            if (buttonY == 3){
+                                buttonX++;
+                                buttonY = 0;
+                            }
                         }
                     }
+                    buttonY = 0;
+                    buttonX = 0;
+                    buttonFound = false;
+                    player1Turn = false;
+                    if (gameBoard.checkWin(player1Symbol)){
+                        gameDone = true;
+                        victor.setText("The winner is " + player1Name + "!");
+                    }
                 }
-                buttonY = 0;
-                buttonX = 0;
-                buttonFound = false;
-                player1Turn = true;
-                if (gameBoard.checkWin(player2Symbol)) gameDone = true;
+                else{
+                    button.setText(String.valueOf(player2Symbol));
+                    while (!buttonFound){
+                        if (button == buttons[buttonX][buttonY]){
+                            gameBoard.play(player2Symbol, buttonX, buttonY);
+                            buttonFound = true;
+                        }
+                        else{
+                            buttonY++;
+                            if (buttonY == 3){
+                                buttonX++;
+                                buttonY = 0;
+                            }
+                        }
+                    }
+                    buttonY = 0;
+                    buttonX = 0;
+                    buttonFound = false;
+                    player1Turn = true;
+                    if (gameBoard.checkWin(player2Symbol)){
+                        gameDone = true;
+                        victor.setText("The winner is " + player2Name + "!");
+                    }
+                }
+                if (gameDone){
+                    JFrame winnerFrame = new JFrame();
+                    JPanel victorPanel = new JPanel();
+                    victorPanel.add(victor);
+                    winnerFrame.add(victorPanel);
+                    Dimension screenDimension = Toolkit.getDefaultToolkit().getScreenSize();
+                    int xMid = (screenDimension.width - 300) / 2;
+                    int yMid = (screenDimension.height - 100) / 2;
+                    winnerFrame.setSize(300,100);
+                    winnerFrame.setLocation(xMid, yMid);
+                    winnerFrame.setVisible(true);
+                }
                 return;
             }
         }
